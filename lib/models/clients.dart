6042a5781afbaee2e68ae3cc57e84e67131df15a
Gisele <gisesonia@gmail.com>
2022-11-30 09:46:31 +0000

    Centralizando acesso aos clientes com uma classe

---
+++ lib/models/clients.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'client.dart';
+
+class Clients extends ChangeNotifier {
+  List<Client> clients;
+
+  Clients({
+    required this.clients
+  })
+}
